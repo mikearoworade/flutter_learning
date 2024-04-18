36c0af18abaa92929d77fb5ddacdcdd694c8ba4f
Michael Aroworade <Ayoaro85@gmail.com>
2024-04-18 14:34:43 +0100

    quiz updated

---
+++ adv_basics/lib/start_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(height: 50),
+          Text(
+            'Learn Flutter the fun way', 
+            style: GoogleFonts.roboto(
+              color: const Color.fromARGB(255, 237, 223, 252),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+              side: const BorderSide(width: 1, color: Colors.white),
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text(
+              'Start Quiz',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
